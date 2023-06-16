471a95a4e466afb87c7f5e1fe8c98f8ac50c9eb4
heunseoRyu <120763372+heunseoRyu@users.noreply.github.com>
2023-06-16 15:01:19 +0900

    getApi
    how to get openweatherAPI

---
+++ howToGetApi.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+city = "Seoul" # 도시
+apiKey = "24aa027670b6c7e617372b3c649ae18b" # API 키
+lang = "kr" # 언어
+units = "metric" # 섭씨 온도로 변경
+
+# API 요청을 보낼 URL 생성
+api = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&lang={lang}&units={units}"
+print(api)
+# API 호출
+response = requests.post(api)
+
+# 응답 결과 확인
+if response.status_code == 200:
+    result = json.loads(response.text)
+    print(result)
+else:
+    print("API 요청에 실패했습니다. 상태 코드:", response.status_code)
